ac8f7a88a816eb4ccef7eb15a0fa2762962d89cb
Nelson Ramirez <xhyskoax@gmail.com>
2024-12-02 19:48:25 -0300

    se actualiza admin.py de laboratorio y se crea requirements.txt

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -4,7 +4,24 @@ from .models import Laboratorio, Producto, DirectorGeneral
 
 # Register your models here.
 
+class LaboratorioAdmin(admin.ModelAdmin):
+    fields = ["nombre"]
+    list_display = ["id", "nombre"]
+    list_display_links = ["nombre"]
+    search_fields = ["nombre"]
+    ordering = ["id"]
+    list_filter = ["nombre"]
+    
+    
+class ProductoAdmin(admin.ModelAdmin):
+    fields = ["nombre"]
+    list_display = ["id", "nombre", "laboratorio", "f_fabricacion", "precio_costo", "precio_venta"]
+    list_display_links = ["nombre", "laboratorio"]
+    search_fields = ["nombre"]
+    ordering = ["id"]
+    list_filter = ["laboratorio"]
+    
 
-admin.site.register(Laboratorio)
-admin.site.register(Producto)
+admin.site.register(Laboratorio, LaboratorioAdmin)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(DirectorGeneral)
